df8a1f3654192d7d6ae60c60325f3f9dbfb5112d
unknown <s.solovev9208@gmail.com>
2020-07-09 17:49:51 +0300

    django test

---
+++ blog/admin.py
@@ -0,0 +1,46 @@
+from django.contrib import admin
+from django.utils.safestring import mark_safe
+from django import forms
+from .models import *
+from ckeditor_uploader.widgets import CKEditorUploadingWidget
+
+
+class PostAdminForm(forms.ModelForm):
+    content = forms.CharField(widget=CKEditorUploadingWidget())
+    class Meta:
+        model = Post
+        fields = '__all__'
+
+
+class PostAdmin(admin.ModelAdmin):
+    prepopulated_fields = {"slug": ('title',)}
+    form = PostAdminForm
+    save_as = True
+    save_on_top = True
+    list_display = ('id', 'title', 'slug', 'category', 'created_at', 'get_photo')
+    list_display_links = ('title', 'id')
+    search_fields = ('title',)
+    list_filter = ('category','tags')
+    readonly_fields = ('views', 'created_at', 'get_photo')
+    fields = ('title', 'slug', 'category', 'tags', 'content', 'photo', 'get_photo', 'views', 'created_at')
+
+    def get_photo(self, obj):
+        if obj.photo:
+            return mark_safe(f'<img src="{obj.photo.url}" width="50">')
+        return '-'
+    get_photo.short_description = 'Фото'
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {"slug": ('title',)}
+
+
+class TagAdmin(admin.ModelAdmin):
+    prepopulated_fields = {"slug": ('title',)}
+
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Tag, TagAdmin)
+admin.site.register(Post, PostAdmin)
+
+
